867ced788ac844f4b11dd65a40ab066c52136212
Gary Sorohan <garysorohan@gmail.com>
2024-05-13 11:27:03 +0000

    set up book_lesson app and test it

--- horse_riding_lessons/urls.py
+++ horse_riding_lessons/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from book_lesson.views import make_booking
 
 urlpatterns = [
+    path('', make_booking, name='make_booking'),
     path('admin/', admin.site.urls),
 ]
