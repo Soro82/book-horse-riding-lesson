7bfda9eba38870d3a24bd77f4966c9b2b8a83812
Gary Sorohan <garysorohan@gmail.com>
2024-05-13 16:00:57 +0000

    set up a secret key for the project

--- horse_riding_lessons/settings.py
+++ horse_riding_lessons/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)tman=-rx0#*^s^4o%7jk&6)p)o6lh*3@+aryu1hx%lx*pq!3@'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
