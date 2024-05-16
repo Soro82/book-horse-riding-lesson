89fd6064051d308c12da5378f81c5f46f4024e8c
Gary Sorohan <garysorohan@gmail.com>
2024-05-16 11:19:20 +0000

    build the horse model and register it

--- book_lesson/models.py
+++ book_lesson/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Horse(models.Model):
+    """
+    Details of each horse available.
+    """
+    name = models.CharField(max_length=20)
+    color = models.CharField(max_length=20)
+    height = models.DecimalField(max_digits=4, decimal_places=2)
+
+    def __str__(self):
+        return self.name
+
+        
