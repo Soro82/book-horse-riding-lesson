998d5f008f5642ab514d7ae8b92ad0d6c53ba347
Gary Sorohan <garysorohan@gmail.com>
2024-05-16 13:53:10 +0000

    build the booking model and register it

--- book_lesson/models.py
+++ book_lesson/models.py
@@ -13,4 +13,23 @@ class Horse(models.Model):
     def __str__(self):
         return self.name
 
+
+class Booking(models.Model):
+    """
+    Book a horse riding lesson.
+    """
+    EXPERIENCE_LEVEL = ((0, "Beginner"), (1, "Intermediate"), (2, "Advanced"))
+    INDOOR_OUTDOOR = ((0, "Indoor"), (1, "Outdoor"))
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    lesson_date = models.DateField()
+    lesson_time = models.TimeField()
+    location = models.IntegerField(choices=INDOOR_OUTDOOR, default=0)
+    experience = models.IntegerField(choices=EXPERIENCE_LEVEL, default=0)
+    horse = models.ForeignKey(Horse, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Booking {self.id} for {self.user.username} on {self.lesson_date}"
+
+
         
