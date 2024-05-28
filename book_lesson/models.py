64a152d83627bb418adb69f9374d04f694af654b
Gary Sorohan <garysorohan@gmail.com>
2024-05-28 19:00:14 +0000

    update project to use cloudinary

--- book_lesson/models.py
+++ book_lesson/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
 from django.contrib.auth.models import User
 
 # Create your models here.
@@ -9,6 +10,7 @@ class Horse(models.Model):
     name = models.CharField(max_length=20)
     color = models.CharField(max_length=20)
     height = models.DecimalField(max_digits=4, decimal_places=2)
+    image = CloudinaryField('image', default='placeholder')
 
     def __str__(self):
         return self.name
