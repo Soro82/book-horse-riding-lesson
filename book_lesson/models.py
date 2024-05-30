d8226cce1535c15cd241044bb5f99213a70783d6
Gary Sorohan <garysorohan@gmail.com>
2024-05-30 15:38:50 +0000

    update available lesson times in Booking model

--- book_lesson/models.py
+++ book_lesson/models.py
@@ -22,10 +22,12 @@ class Booking(models.Model):
     """
     EXPERIENCE_LEVEL = ((0, "Beginner"), (1, "Intermediate"), (2, "Advanced"))
     INDOOR_OUTDOOR = ((0, "Indoor"), (1, "Outdoor"))
+    LESSON_TIMES = ((0, "10:00"), (1, "11:00"), (2, "12:00"), (3, "14:00"),
+     (4, "15:00"), (5, "16:00"), (6, "17:00"))
 
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     lesson_date = models.DateField()
-    lesson_time = models.TimeField()
+    lesson_time = models.TimeField(choices=LESSON_TIMES, default=0)
     location = models.IntegerField(choices=INDOOR_OUTDOOR, default=0)
     experience = models.IntegerField(choices=EXPERIENCE_LEVEL, default=0)
     horse = models.ForeignKey(Horse, on_delete=models.CASCADE)
