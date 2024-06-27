8582c7d4e2017fd52df0e8c5bfbc2ad905ad4c1b
Gary Sorohan <garysorohan@gmail.com>
2024-06-27 10:18:11 +0000

    add filter to bookings in admin panel

--- book_lesson/admin.py
+++ book_lesson/admin.py
@@ -2,4 +2,13 @@ from django.contrib import admin
 from .models import Horse, Booking
 
 admin.site.register(Horse)
-admin.site.register(Booking)
+
+
+@admin.register(Booking)
+class Bookings(admin.ModelAdmin):
+    """
+    Arranges how the Bookings are displayed for the admin user.
+    Allows the admin user to filter the list of bookings.
+    """
+    list_display = ('user', 'horse', 'lesson_date', 'lesson_time')
+    list_filter = ('user', 'horse')
