e0732dd9953ff6c2fa03d33a67fe30b66d2eb02e
Gary Sorohan <garysorohan@gmail.com>
2025-05-02 16:44:13 +0000

    Update debug to False

--- horse_riding_lessons/settings.py
+++ horse_riding_lessons/settings.py
@@ -27,12 +27,12 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['8000-soro82-bookhorseridingl-rd3kqlxm67c.ws.codeinstitute-ide.net','.herokuapp.com']
 
 
 # Application definition
